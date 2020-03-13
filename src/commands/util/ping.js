8b3042b3e019c6346884c705700c7a7d3192bc85
Pedro Lopez <pedroslopez@me.com>
2020-03-13 18:15:43 -0400

    add timing functionality to default ping command

--- src/commands/util/ping.js
+++ src/commands/util/ping.js
@@ -4,13 +4,29 @@ class PingCommand extends Command {
 	constructor(commander) {
         super(commander, {
             name: 'ping',
-            aliases: ['check'],
-            description: 'Checks if the bot is online'
-        })
+            aliases: ['latency'],
+            description: 'Pings and checks the time it takes for the message to be sent to the server.'
+        });
+
+        commander.client.on('message_ack', this.onAck.bind(this));
+        this.awaitingAck = {};
     }
 
-    run(message) {
-        message.reply('pong');
+    async onAck(message, ack) {
+        let sentTime = this.awaitingAck[message.id._serialized];
+
+        if(ack > 0 && sentTime) {
+            delete this.awaitingAck[message.id._serialized];
+            
+            let ms = new Date().getTime() - sentTime;
+            message.reply(`Ping took ${ms}ms`);
+        }
+    }
+
+    async run(message) {
+        let chat = await message.getChat();
+        let pingMsg = await chat.sendMessage('Pinging...');
+        this.awaitingAck[pingMsg.id._serialized] = new Date().getTime();
     }
 };
 
